9b0da98e5e9b8f144b68e92500b336b6a56f628e
Noor Al Darraji <077068@yahoo.com>
2019-12-03 20:23:50 -0600

    Intro to Python I practice :fire:

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,7 +1,16 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
-
+def is_even():
+    okay = int(input('enter int: '))
+    even = (okay % 2) == 0
+    if even:
+        print('Even')
+        return True
+    else:
+        print('Odd')
+        return False
+is_even()
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -9,4 +18,4 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+#is_even()
